9ef3b0ec08af697953385024ea560df29b35b63a
Becczs <rebecca.anderssoncarlsson@gmail.com>
2025-03-22 10:02:41 +0100

    API axios

--- src/utils/api.js
+++ src/utils/api.js
@@ -1,24 +1,56 @@
 
-export function getBaseUrl() {
-  // Get the group number from the hostname to determine the base URL for BE
-  const regex = /webshop\-2025\-(g[0-9]{1,2})\-fe/g;
-  const href = window.location.href;
-  const match = regex.exec(href);
-  console.log(match);
-  if (match) {
-    const group = match[1];
-    return `https://webshop-2025-be-g9.vercel.app/api/products`;
-  }
-  return "http://localhost:3000/";
-}
+// export function getBaseUrl() {
+//   // Get the group number from the hostname to determine the base URL for BE
+//   const regex = /webshop\-2025\-(g[0-9]{1,2})\-fe/g;
+//   const href = window.location.href;
+//   const match = regex.exec(href);
+//   console.log(match);
+//   if (match) {
+//     const group = match[1];
+//     return `https://webshop-2025-be-g9.vercel.app/api/products`;
+//   }
+//   return "http://localhost:3000/";
+// }
 
-export async function fetchProducts(endpoint = "api/products") {
-  //! DONT USE THIS IN PRODUCTION
-  const url = `${getBaseUrl()}${endpoint}`;
-  const response = await fetch(url);
-  if(response.ok){
-    const data = await response.json();
-    return data;
+// export async function fetchProducts(endpoint = "api/products") {
+//   //! DONT USE THIS IN PRODUCTION
+//   const url = `${getBaseUrl()}${endpoint}`;
+//   const response = await fetch(url);
+//   if(response.ok){
+//     const data = await response.json();
+//     return data;
+//   }
+//   return [];    
+// }
+
+
+
+
+
+
+//fetch
+
+// export async function fetchProducts() {
+//   const url = 'https://webshop-2025-be-g9.vercel.app/api/products';
+//   const response = await fetch(url);
+//   if (response.ok) {
+//     const data = await response.json();
+//     return data;
+//   }
+//   return [];
+// }
+
+
+
+//axios
+
+export async function fetchProducts() {
+  const url = 'https://webshop-2025-be-g9.vercel.app/api/products';
+  try {
+    const response = await axios.get(url);
+    return response.data;
+  } catch (error) {
+    console.error('Fel vid hämtning av produkter:', error);
+    return [];
   }
-  return [];    
 }
